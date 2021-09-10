e456aed53f5d62072e71953e56e700ab4cde332c
Jiseok97 <wltjr1426@naver.com>
2021-09-10 14:22:40 +0900

    Wrap up getting started

---
+++ Messenger/Controllers/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by 이지석 on 2021/09/10.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
